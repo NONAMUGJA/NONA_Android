b4f2a19a7eaea129cef5469c6eadbf0477ee4f55
KimGGun <rlarjs2020@dgsw.hs.kr>
2018-06-13 01:32:08 +0900

    1:30 merge

--- app/src/main/java/com/dgsw/nona/Adapter/AlarmAdapter.java
+++ app/src/main/java/com/dgsw/nona/Adapter/AlarmAdapter.java
@@ -1,3 +1,3 @@
 package com.dgsw.nona.Adapter;
 
-public class AlarmAdapter {String string; String from; }
+
--- app/src/main/java/com/dgsw/nona/Model/Alarm.java
+++ app/src/main/java/com/dgsw/nona/Model/Alarm.java
@@ -1,1 +1,10 @@
 package com.dgsw.nona.Model;
+
+class Alarm{
+    String comment;
+    String target;
+    public Alarm (String comment, String target){
+        this.comment = comment;
+        this.target = target;
+    }
+}
